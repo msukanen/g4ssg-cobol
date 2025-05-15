fd63b20203e0fce8dcd3eca3caf6d753a99a7b41
msukanen <anikaiful@gmail.com>
2025-05-15 04:34:05 +0300

    Stellar age and population.

--- MAIN.cob
+++ MAIN.cob
@@ -15,7 +15,8 @@
       * Random number generation:
       *********
       *01  RND-SEED-VALUE          PIC 9(4) COMP-5 VALUE 0.
-       01  RND-INT                 PIC 9(5).
+       01  DICEROLL                PIC 9(5).
+       01  TMP-INT                 PIC 9(5).
        01  WS-NUM-DICE             PIC S9(1) COMP-5.
       *********
       * Parsed run params:
@@ -31,9 +32,14 @@
       *********    
        01  MASS-INDEX-OFFSET    PIC 99.
        01  STAR-INDEX           PIC 99.
+       01  CREATING-COMPANION   PIC X VALUE 'N'.
        01  STAR-SYSTEM.
            05 IN-CLUSTER-OR-CORE   PIC X VALUE 'N'.
-           05 STAR-SYSTEM-NAME     PIC x(48).
+           05 STAR-SYSTEM-NAME     PIC X(48).
+      *       Stellar populations, in order of general age:
+      *         E1, Y1, I1, O1, I2, E2
+           05 STELLAR-POPULATION   PIC XX.
+           05 STELLAR-AGE          PIC 9(2)V9(2).
            05 NUM-OF-STARS         PIC 99.
            05 STAR OCCURS 1 TO 10 TIMES DEPENDING ON NUM-OF-STARS.
       * A-Z/##/###, or SPACE
@@ -87,11 +93,11 @@
       * Determine number of stars.
       *********
        DETERMINE-NUM-STARS.
-           CALL '3D6' USING RND-INT.
+           CALL '3D6' USING DICEROLL.
            EVALUATE TRUE
-               WHEN RND-INT IS LESS OR EQUAL TO 10
+               WHEN DICEROLL IS LESS OR EQUAL TO 10
                    MOVE 1 TO NUM-OF-STARS
-               WHEN RND-INT IS LESS OR EQUAL TO 15
+               WHEN DICEROLL IS LESS OR EQUAL TO 15
                    MOVE 2 TO NUM-OF-STARS
                WHEN OTHER
                    MOVE 3 TO NUM-OF-STARS
@@ -103,49 +109,61 @@
       * Determine mass index.
       *********
        DETERMINE-MASS-INDEX.
-           CALL '3D6' USING RND-INT.
+           CALL '3D6' USING DICEROLL.
            EVALUATE TRUE
-               WHEN RND-INT = 3
+               WHEN DICEROLL = 3
                    MOVE 0 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-2
-               WHEN RND-INT = 4
+               WHEN DICEROLL = 4
                    MOVE 2 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-3
-               WHEN RND-INT = 5
+               WHEN DICEROLL = 5
                    MOVE 5 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-4
-               WHEN RND-INT = 6
+               WHEN DICEROLL = 6
                    MOVE 9 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-5
-               WHEN RND-INT = 7
+               WHEN DICEROLL = 7
                    MOVE 14 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-5
-               WHEN RND-INT = 8
+               WHEN DICEROLL = 8
                    MOVE 19 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-5
-               WHEN RND-INT = 9
+               WHEN DICEROLL = 9
                    MOVE 24 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-3
-               WHEN RND-INT = 10
+               WHEN DICEROLL = 10
                    MOVE 27 TO MASS-INDEX-OFFSET
                    PERFORM DETERMINE-MASS-INDEX-3
-               WHEN RND-INT = 11
+               WHEN DICEROLL = 11
                    MOVE 30 TO MASS-INDEX(STAR-INDEX)
-               WHEN RND-INT = 12
+               WHEN DICEROLL = 12
                    MOVE 31 TO MASS-INDEX(STAR-INDEX)
-               WHEN RND-INT = 13
+               WHEN DICEROLL = 13
                    MOVE 32 TO MASS-INDEX(STAR-INDEX)
                WHEN OTHER
                    MOVE 33 TO MASS-INDEX(STAR-INDEX)
            END-EVALUATE
+
+      *    If creating a companion star, we step mass-index deeper by
+      *    1d6-1 steps (capping to 33).
+           IF CREATING-COMPANION = 'Y' THEN
+               CALL '1D6' USING DICEROLL
+               COMPUTE TMP-INT = MASS-INDEX(STAR-INDEX) + DICEROLL - 1
+               IF TMP-INT > 33 THEN
+                   MOVE 33 TO TMP-INT
+               END-IF
+               MOVE TMP-INT TO MASS-INDEX(STAR-INDEX)
+               MOVE 'N' TO CREATING-COMPANION
+           END-IF
            GOBACK.
 
       *********
       * Determine mass index; X choices.
       *********
        DETERMINE-MASS-INDEX-2.
-           CALL '3D6' USING RND-INT.
-           IF RND-INT IS LESS OR EQUAL TO 10
+           CALL '3D6' USING DICEROLL.
+           IF DICEROLL IS LESS OR EQUAL TO 10
                MOVE 0 TO MASS-INDEX(STAR-INDEX)
            ELSE
                MOVE 1 TO MASS-INDEX(STAR-INDEX)
@@ -153,12 +171,12 @@
            GOBACK.
 
        DETERMINE-MASS-INDEX-3.
-           CALL '3D6' USING RND-INT.
+           CALL '3D6' USING DICEROLL.
            EVALUATE TRUE
-               WHEN RND-INT IS LESS OR EQUAL TO 8
+               WHEN DICEROLL IS LESS OR EQUAL TO 8
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 0 + MASS-INDEX-OFFSET
-               WHEN RND-INT IS LESS OR EQUAL TO 11
+               WHEN DICEROLL IS LESS OR EQUAL TO 11
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 1 + MASS-INDEX-OFFSET
                WHEN OTHER
@@ -168,15 +186,15 @@
            GOBACK.
 
        DETERMINE-MASS-INDEX-4.
-           CALL '3D6' USING RND-INT.
+           CALL '3D6' USING DICEROLL.
            EVALUATE TRUE
-               WHEN RND-INT IS LESS OR EQUAL TO 7
+               WHEN DICEROLL IS LESS OR EQUAL TO 7
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 0 + MASS-INDEX-OFFSET
-               WHEN RND-INT IS LESS OR EQUAL TO 10
+               WHEN DICEROLL IS LESS OR EQUAL TO 10
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 1 + MASS-INDEX-OFFSET
-               WHEN RND-INT IS LESS OR EQUAL TO 12
+               WHEN DICEROLL IS LESS OR EQUAL TO 12
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 2 + MASS-INDEX-OFFSET
                WHEN OTHER
@@ -186,18 +204,18 @@
            GOBACK.
 
        DETERMINE-MASS-INDEX-5.
-           CALL '3D6' USING RND-INT.
+           CALL '3D6' USING DICEROLL.
            EVALUATE TRUE
-               WHEN RND-INT IS LESS OR EQUAL TO 7
+               WHEN DICEROLL IS LESS OR EQUAL TO 7
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 0 + MASS-INDEX-OFFSET
-               WHEN RND-INT IS LESS OR EQUAL TO 9
+               WHEN DICEROLL IS LESS OR EQUAL TO 9
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 1 + MASS-INDEX-OFFSET
-               WHEN RND-INT = 10
+               WHEN DICEROLL = 10
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 2 + MASS-INDEX-OFFSET
-               WHEN RND-INT IS LESS OR EQUAL TO 12
+               WHEN DICEROLL IS LESS OR EQUAL TO 12
                    COMPUTE MASS-INDEX(STAR-INDEX)
                        = 3 + MASS-INDEX-OFFSET
                WHEN OTHER
@@ -283,7 +301,7 @@
            GOBACK.
 
       *********
-      * Determine ordering based on star-index.
+      * Determine star ordering in the system based on star-index(es).
       *********
        DETERMINE-ORDERING.
            IF NUM-OF-STARS = 1 THEN
@@ -321,4 +339,43 @@
                WHEN STAR-INDEX = 26 MOVE 'Z' TO ORDERING(STAR-INDEX)
       D        WHEN OTHER MOVE '<U>' TO ORDERING(STAR-INDEX)
            END-EVALUATE.
+           GOBACK.
+
+      *********
+      * Determine stellar age and population of the star system.
+      *********
+       DETERMINE-STELLAR-AGE.
+           CALL '3D6' USING DICEROLL.
+           IF DICEROLL = 3 THEN
+               MOVE 'E1' TO STELLAR-POPULATION
+               MOVE 0.0 TO STELLAR-AGE
+           ELSE
+               CALL '1D6' USING DICEROLL
+               CALL '1D6' USING TMP-INT
+               IF DICEROLL IS LESS OR EQUAL TO 6
+                   MOVE 'Y1' TO STELLAR-POPULATION
+                   COMPUTE STELLAR-AGE
+                           = DICEROLL * 0.3
+                           + TMP-INT * 0.05
+                           + 0.1
+               ELSE
+                   COMPUTE STELLAR-AGE
+                           = DICEROLL * 0.6
+                           + TMP-INT * 0.1
+                   EVALUATE TRUE
+                       WHEN DICEROLL IS LESS OR EQUAL TO 10
+                           MOVE 'I1' TO STELLAR-POPULATION
+                           COMPUTE STELLAR-AGE = STELLAR-AGE + 2.0
+                       WHEN DICEROLL IS LESS OR EQUAL TO 14
+                           MOVE 'O1' TO STELLAR-POPULATION
+                           COMPUTE STELLAR-AGE = STELLAR-AGE + 5.6
+                       WHEN DICEROLL IS LESS OR EQUAL TO 17
+                           MOVE 'I2' TO STELLAR-POPULATION
+                           COMPUTE STELLAR-AGE = STELLAR-AGE + 8.0
+                       WHEN OTHER
+                           MOVE 'E2' TO STELLAR-POPULATION
+                           COMPUTE STELLAR-AGE = STELLAR-AGE + 10.0
+                   END-EVALUATE
+               END-IF
+           END-IF.
            GOBACK.
